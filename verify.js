3561ac57caac535c85ad62e30544f1565188fe5e
skullkid2424 <skullkid2424@gmail.com>
2020-06-21 04:51:14 -0400

    Add verify.js file for verifying defense

---
+++ verify.js
@@ -0,0 +1,26 @@
+// Javascript to verify that a setup is valid,
+// Given an array of tiles, output to a text box errors (red) and warnings (yellow)
+
+// Invalid tiles per terrain (EZ Coords)
+//springwater = [18, 25]
+//wintry = [14,29,34]
+//abandoned_castle = [13,26,27,29]
+//snowdust = [18,20,25,33]
+//desert = [15,33,34]
+//spring_breeze = [13,16,33,36]
+//leafy_canopy = [16,17,26,27]
+//lost_castle = [14,15,20,30]
+//bright_grassland = [16,17,35]
+//lava_floes = [18,27,33]
+
+
+
+// TODO: Verify each building only usedonce
+// TODO: Verify Fort and pots exist
+// TODO: Verify no more than 6 heroes
+// TODO: Warn if less than 6 heroes
+// TODO: Warn if heroes below top 2 rows
+// TODO: Verify traps (2 of each type, just in case they want to use double fake traps to "hide" the real one)
+// TODO: Verify no buildings on invalid spaces for the terrain
+
+// TODO: Verify function that runs through all functions and outputs results
